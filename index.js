80df9b755847ba743de337369c1b41d17cabd97e
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-12-07 00:41:39 -0600

    added keydown event listener

--- index.js
+++ index.js
@@ -9,36 +9,43 @@ let tom2 = new Audio("sounds/tom-2.mp3")
 let tom3 = new Audio("sounds/tom-3.mp3")
 let tom4 = new Audio("sounds/tom-4.mp3")
 
+function makeSound(char) {
+	switch (char) {
+		case "w":
+			crash.play();
+			break;
+		case "a":
+			kickBass.play();
+			break;
+		case "s":
+			snare.play();
+			break;
+		case "d":
+			tom1Drum.play();
+			break;
+		case "j":
+			tom2.play();
+			break;
+		case "k":
+			tom3.play();
+			break;
+		case "l":
+			tom4.play();
+			break;
+		default:
+			console.log(char)
+	}
+	
+}
 
 for (let i = 0; i < numOfDrums; i++) {
 	document.querySelectorAll('.drum')[i].addEventListener('click', (function () {
 		// this.style.color = "white";
 		let buttonInnerHTML = this.innerHTML;
-		switch (buttonInnerHTML) {
-			case "w":
-				crash.play();
-				break;
-			case "a":
-				kickBass.play();
-				break;
-			case "s":
-				snare.play();
-				break;
-			case "d":
-				tom1Drum.play();
-				break;
-			case "j":
-				tom2.play();
-				break;
-			case "k":
-				tom3.play();
-				break;
-			case "l":
-				tom4.play();
-				break;
-			default:
-				console.log(buttonInnerHTML)
-		}
+		makeSound(buttonInnerHTML)
 	}))
 }
 
+document.addEventListener("keydown", function (e) {
+	makeSound(e.key)
+})
